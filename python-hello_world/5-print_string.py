746a4a0e77d4390f787b5eb7e686738329792e13
Izzat <shixbalayevizzet@example.com>
2026-03-20 19:35:02 +0000

    task3

--- python-hello_world/5-print_string.py
+++ python-hello_world/5-print_string.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 str = "Holberton"
-# print 3 times the string
-print(str * 3)
-# print first 9 characters
-print(str[:9])
+print(str * 3)  # print 3 times the string
+print(str[:9])  # print first 9 characters

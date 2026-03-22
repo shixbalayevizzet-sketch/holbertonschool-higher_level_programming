11f5de5e119e1deb9d88b23e06045192f4978b0a
Izzat <shixbalayevizzet@example.com>
2026-03-22 18:03:35 +0000

    task8

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,6 +1,9 @@
+#!/usr/bin/python3
 def uppercase(str):
     for char in str:
+        # If the character is between 'a' (97) and 'z' (122)
         if ord(char) >= 97 and ord(char) <= 122:
+            # Shift it to the uppercase range
             char = chr(ord(char) - 32)
         print("{}".format(char), end="")
     print("")

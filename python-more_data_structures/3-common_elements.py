c2c410f7e2a3100978d71a5dd02872730a1a49cd
Izzat <shixbalayevizzet@example.com>
2026-04-01 20:26:22 +0000

    task3

---
+++ python-more_data_structures/3-common_elements.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def common_elements(set_1, set_2):
+    """
+    Returns a set of common elements in two sets.
+    """
+    # Use the intersection operator (&) to find common elements
+    return set_1 & set_2

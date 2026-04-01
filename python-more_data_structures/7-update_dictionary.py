0683aa580adf3d522bc7c09d6f9b8ce235c9e631
Izzat <shixbalayevizzet@example.com>
2026-04-01 20:46:46 +0000

    task7

---
+++ python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def update_dictionary(a_dictionary, key, value):
+    """
+    Replaces or adds key/value in a dictionary.
+    """
+    # Simply assigning the value to the key handles both 
+    # replacing an existing key and adding a new one.
+    a_dictionary[key] = value
+    return a_dictionary

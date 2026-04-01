a046b73bd7e7319c65e35d8fc0ac9a8115920756
Izzat <shixbalayevizzet@example.com>
2026-04-01 20:42:08 +0000

    task6

---
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def number_keys(a_dictionary):
+    """
+    Returns the number of keys in a dictionary.
+    """
+    # len() function returns the count of items (keys) in the dictionary
+    return len(a_dictionary)

e643ce35f89e230367c53a360153bfe218e6b6e0
Izzat <shixbalayevizzet@example.com>
2026-04-11 20:46:13 +0000

    task2

---
+++ python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    printed_count = 0
+    for i in range(x):
+        try:
+            print("{:d}".format(my_list[i]), end="")
+            printed_count += 1
+        except (ValueError, TypeError):
+            continue
+        except IndexError:
+            break
+    print("")  # Required new line
+    return printed_count

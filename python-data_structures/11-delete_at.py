e7248c00b48e87a96fab1bbc0ac16dd2b84f811b
Izzat <shixbalayevizzet@example.com>
2026-04-24 19:21:55 +0000

    task11

---
+++ python-data_structures/11-delete_at.py
@@ -0,0 +1,11 @@
+def delete_at(my_list=[], idx=0):
+    """
+    Siyahıda müəyyən edilmiş indeksdəki elementi silir.
+    """
+    # İndeksin mənfi və ya diapazondan kənar olub-olmadığını yoxlayırıq
+    if idx < 0 or idx >= len(my_list):
+        return my_list
+
+    # del operatoru siyahıdakı elementi verilmiş indeks üzrə silir
+    del my_list[idx]
+    return my_list

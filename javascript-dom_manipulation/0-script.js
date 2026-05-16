a265e80f33f9355c3bdd324e06aadc231082cc15
Izzat <shixbalayevizzet@example.com>
2026-05-16 19:18:25 +0000

    task0

---
+++ javascript-dom_manipulation/0-script.js
@@ -0,0 +1,7 @@
+// Select the first <header> element on the page
+const headerElement = document.querySelector('header');
+
+// If the element exists, change its text color to red (#FF0000)
+if (headerElement) {
+    headerElement.style.color = '#FF0000';
+}

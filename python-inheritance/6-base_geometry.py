d8df741f345650a3731e52e8d5b33157b3b6bdee
Izzat <shixbalayevizzet@example.com>
2026-06-04 14:37:39 +0000

    task6

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""BaseGeometry class with area method."""
+
+
+class BaseGeometry:
+    """BaseGeometry class."""
+
+    def area(self):
+        """Raises an Exception indicating area is not implemented."""
+        raise Exception("area() is not implemented")

fc10ed37164191151a501a315365a33e6c566963
Izzat <shixbalayevizzet@example.com>
2026-06-05 10:36:43 +0000

    task0

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-"""Module that contains the add_integer function."""
-def add_integer(a, b=98):
-    """Add two integers."""
+"""Module containing add_integer function."""
 
+
+def add_integer(a, b=98):
+    """Add two integers after casting floats to integers."""
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
 

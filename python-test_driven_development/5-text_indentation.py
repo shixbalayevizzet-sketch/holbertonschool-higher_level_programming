1e40eee6c45fc193239a6c7497b8d7ea2a156459
Izzat <shixbalayevizzet@example.com>
2026-06-06 13:48:23 +0000

    task5

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
 """Module that contains text_indentation function"""
+
+
 def text_indentation(text):
-    """Prints a text with 2 new lines after each of these characters: ., ? and :"""
+    """
+    Prints a text with 2 new lines after each of these characters: ., ? and :
+    """
     if not isinstance(text, str):
         raise TypeError("text must be a string")
 

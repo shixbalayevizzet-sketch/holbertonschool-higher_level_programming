453d1591858a82efa63b1401cd2f2ac8e36c8e7e
Izzat <shixbalayevizzet@example.com>
2026-06-06 13:50:15 +0000

    task5

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -3,9 +3,7 @@
 
 
 def text_indentation(text):
-    """
-    Prints a text with 2 new lines after each of these characters: ., ? and :
-    """
+    """Prints a text with 2 new lines after each of these characters: ., ? and :"""
     if not isinstance(text, str):
         raise TypeError("text must be a string")
 
@@ -14,8 +12,7 @@ def text_indentation(text):
         if flag == 1:
             if char == " ":
                 continue
-            else:
-                flag = 0
+            flag = 0
         print(char, end="")
         if char in ".?:":
             print("\n")
